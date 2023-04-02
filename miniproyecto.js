f5dd8dce7f67bfcb7ce3ed740e887c340d6487a2
Joaquin Chaile <joachaile23@hotmail.com>
2023-04-02 11:11:43 -0300

    Reinicio de contador

--- miniproyecto.js
+++ miniproyecto.js
@@ -3,25 +3,42 @@ const aumentar = document.getElementById("aumentar")
 aumentar.addEventListener("click", Sumar)
 const disminuir = document.getElementById("disminuir")
 disminuir.addEventListener("click", Restar)
+const reiniciarContador = document.getElementById("reiniciar")
+reiniciarContador.addEventListener("click", Reiniciar)
 
 let conteo = 0
 
 function Sumar()
 {
     conteo = conteo + 1
-    contador.innerHTML = conteo
-    if(conteo >= 1)
+    if(conteo === 0)
+    {
+        contador.style.color = 'black'
+    }
+    else if(conteo >= 1)
     {
         contador.style.color = 'green'
     }
+    contador.innerHTML = conteo
 }
 
 function Restar()
 {
     conteo = conteo - 1
-    contador.innerHTML = conteo
-    if(conteo < 0)
+    if(conteo === 0)
+    {
+        contador.style.color = 'black'
+    }
+    else if(conteo <= -1)
     {
         contador.style.color = 'red'
     }
+    contador.innerHTML = conteo
+}
+
+function Reiniciar()
+{
+    conteo = 0
+    contador.style.color = 'black'
+    contador.innerHTML = 0
 }
